9bc3586b10430ce77c1ebb2de0db43c992b9bd2a
heyjinkim <kim.heyjin@gmail.com>
2014-05-28 08:38:19 -0400

    updates

---
+++ js/data.js
@@ -0,0 +1,14 @@
+var DATA = [
+  { 'letter':'A', 'frequency':1492 },
+  { 'letter':'B', 'frequency':8167 },
+  { 'letter':'C', 'frequency':2780 },
+  { 'letter':'D', 'frequency':4253 },
+  { 'letter':'E', 'frequency':12702 }
+];
+
+var DATA2 = [
+  { 'letter':'A', 'frequency':1492 },
+  { 'letter':'B', 'frequency':12702 },
+  { 'letter':'C', 'frequency':4253 }
+];
+
